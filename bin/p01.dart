6ec2b52338380678d37dd56ff9d7d4364651bb73
mdawlatshah <adawlatshah@gmail.com>
2021-11-19 14:30:00 +0600

    initial commit

---
+++ bin/p01.dart
@@ -0,0 +1,9 @@
+void main() {
+  String message = "Hello, World";
+
+  print(message);
+
+  print(12.5 / 2);
+
+  print(true);
+}
---
+++ bin/p02.dart
@@ -0,0 +1,13 @@
+void main() {
+  int a = 10;
+  int b = 5;
+
+  var age = 15;
+
+  String name = "Tom";
+
+  String dept = "ICP";
+
+  print("Name: " + name);
+  print("Department: " + dept);
+}
